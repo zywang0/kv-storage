4f805d6a1852a0c7e913844096953cdb6e3befcd
zywang0 <13679251160@163.com>
2023-12-04 00:11:41 -0800

    Feat: Implemented ReadLogRecord function.

--- data/data_file.go
+++ data/data_file.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"kv-project/fio"
 	"path/filepath"
 )
@@ -24,6 +26,62 @@ func OpenDataFile(dirPath string, fileID uint32) (*File, error) {
 	return &File{FileID: fileID, WriteOffset: 0, IOManager: ioManager}, nil
 }
 
+// ReadLogRecord read record at offset and return record and its size
+func (f *File) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
+	fileSize, err := f.IOManager.Size()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	//header is read according to the max Header Size
+	var logRecordHeaderSize int64 = maxLogRecordHeaderSize
+
+	//if exceed file size, we will handle EOF and only read to the end of the file
+	if offset+maxLogRecordHeaderSize > fileSize {
+		logRecordHeaderSize = fileSize - offset
+	}
+
+	headerBuff, err := f.ReadNBytes(logRecordHeaderSize, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	//get final real header and its size
+	header, headerSize := DecodeLogRecord(headerBuff)
+
+	//edge case
+	if header == nil {
+		return nil, 0, io.EOF
+	}
+	if header.crc == 0 && header.keySize == 0 && header.valueSize == 0 {
+		return nil, 0, io.EOF
+	}
+
+	keySize := int64(header.keySize)
+	valueSize := int64(header.valueSize)
+
+	var recordSize = headerSize + keySize + valueSize
+
+	record := &LogRecord{Type: header.recordType}
+
+	//read real key and value
+	if keySize > 0 || valueSize > 0 {
+		kvBuff, err := f.ReadNBytes(keySize+valueSize, offset+headerSize)
+		if err != nil {
+			return nil, 0, err
+		}
+		record.Key = kvBuff[:keySize]
+		record.Value = kvBuff[:valueSize]
+	}
+
+	recordCRC := GetRecordCRC(record, headerBuff)
+	if recordCRC != header.crc {
+		return nil, 0, errors.New("invalid crc value")
+	}
+
+	return record, recordSize, nil
+}
+
 func (f *File) Sync() error {
 	return f.IOManager.Sync()
 }
@@ -39,4 +97,14 @@ func (f *File) Write(buf []byte) error {
 
 func (f *File) Close() error {
 	return f.IOManager.Close()
+}
+
+// ReadNBytes convert size from int64 to []byte so that read n size bytes at offset
+func (f *File) ReadNBytes(size int64, offset int64) ([]byte, error) {
+	bytes := make([]byte, size)
+	_, err := f.IOManager.Read(bytes, offset)
+	if err != nil {
+		return nil, err
+	}
+	return bytes, err
 }
--- data/log_records.go
+++ data/log_records.go
@@ -1,5 +1,7 @@
 package data
 
+import "encoding/binary"
+
 type LogRecordType = byte
 
 const (
@@ -23,8 +25,28 @@ type LogRecordPos struct {
 	Size uint32
 }
 
+// Header: crc + type + key_size + value_size
+const maxLogRecordHeaderSize = 4 + 1 + binary.MaxVarintLen32*2
+
+type LogRecordHeader struct {
+	crc        uint32
+	recordType LogRecordType
+	keySize    uint32
+	valueSize  uint32
+}
+
 // EncodeLogRecord return byte array and its size
 func EncodeLogRecord(record *LogRecord) ([]byte, int64) {
 	// TODO
 	return nil, 0
+}
+
+func DecodeLogRecord(buff []byte) (*LogRecordHeader, int64) {
+	// TODO
+	return nil, 0
+}
+
+func GetRecordCRC(record *LogRecord, header []byte) uint32 {
+	// TODO
+	return 0
 }
