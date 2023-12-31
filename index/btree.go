103848b886143e288e462e4be2f42c7b558be767
zywang0 <13679251160@163.com>
2023-12-30 23:05:24 -0800

    Feat: Implemented Index Iterator.

--- index/btree.go
+++ index/btree.go
@@ -1,8 +1,10 @@
 package index
 
 import (
+	"bytes"
 	"github.com/google/btree"
 	"kv-project/data"
+	"sort"
 	"sync"
 )
 
@@ -37,4 +39,79 @@ func (bt *BTree) Delete(key []byte) bool {
 		return false
 	}
 	return true
+}
+func (bt *BTree) Size() int {
+	return bt.tree.Len()
+}
+func (bt *BTree) Iterator(reverse bool) Iterator {
+	if bt.tree == nil {
+		return nil
+	}
+	bt.lock.RLock()
+	defer bt.lock.RUnlock()
+	return newBTreeIterator(bt.tree, reverse)
+}
+
+// BTreeIterator represents an iterator for BTree index.
+type BTreeIterator struct {
+	currIndex int     // Current index position during iteration
+	reverse   bool    // Whether it is a reverse iteration
+	values    []*Item // Key + position index information
+}
+
+func newBTreeIterator(tree *btree.BTree, reverse bool) *BTreeIterator {
+	var idx int
+	values := make([]*Item, tree.Len())
+
+	// Store all data in an array
+	saveValues := func(item btree.Item) bool {
+		values[idx] = item.(*Item)
+		idx++
+		return true
+	}
+	if reverse {
+		tree.Descend(saveValues)
+	} else {
+		tree.Ascend(saveValues)
+	}
+
+	return &BTreeIterator{
+		currIndex: 0,
+		reverse:   reverse,
+		values:    values,
+	}
+}
+
+func (bti *BTreeIterator) Rewind() {
+	bti.currIndex = 0
+}
+func (bti *BTreeIterator) Seek(key []byte) {
+	if bti.reverse {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) <= 0
+		})
+	} else {
+		bti.currIndex = sort.Search(len(bti.values), func(i int) bool {
+			return bytes.Compare(bti.values[i].key, key) >= 0
+		})
+	}
+}
+func (bti *BTreeIterator) Next() {
+	bti.currIndex += 1
+}
+
+func (bti *BTreeIterator) Valid() bool {
+	return bti.currIndex < len(bti.values)
+}
+
+func (bti *BTreeIterator) Key() []byte {
+	return bti.values[bti.currIndex].key
+}
+
+func (bti *BTreeIterator) Value() *data.LogRecordPos {
+	return bti.values[bti.currIndex].pos
+}
+
+func (bti *BTreeIterator) Close() {
+	bti.values = nil
 }
--- index/index.go
+++ index/index.go
@@ -11,6 +11,10 @@ type Indexer interface {
 	Put(key []byte, pos *data.LogRecordPos) bool
 	Get(key []byte) *data.LogRecordPos
 	Delete(key []byte) bool
+	// Size returns the number of entries in the index
+	Size() int
+	// Iterator returns an iterator for the index
+	Iterator(reverse bool) Iterator
 }
 
 type IndexerType = int8
@@ -39,4 +43,28 @@ type Item struct {
 
 func (ai *Item) Less(bi btree.Item) bool {
 	return bytes.Compare(ai.key, bi.(*Item).key) == -1
+}
+
+// Iterator General index iterator
+type Iterator interface {
+	// Rewind resets the iterator to the beginning, i.e., the first entry
+	Rewind()
+
+	// Seek seeks to a target key that is >= or <= the given key
+	Seek(key []byte)
+
+	// Next moves to the next key.
+	Next()
+
+	// Valid returns whether the iterator is still valid, i.e., if all keys have been traversed
+	Valid() bool
+
+	// Key returns the key at the current iterator position.
+	Key() []byte
+
+	// Value returns the value (position information) at the current iterator position.
+	Value() *data.LogRecordPos
+
+	// Close closes the iterator and releases any resources.
+	Close()
 }
